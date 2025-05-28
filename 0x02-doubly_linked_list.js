10a779e16ef952c3d937fc33cb3fa0f85975fd17
Edwin Irungu <irungundigirigi@gmail.com>
2025-05-28 12:49:33 +0300

    implement insert node at index 0

--- 0x02-doubly_linked_list.js
+++ 0x02-doubly_linked_list.js
@@ -18,6 +18,7 @@ class doubly_linked_list{
         this.size = 0
     }
 
+    /** adds a node to the end of list */
     append_node(value) {
         let new_node = new Node(value);
         if(!this.head){
@@ -29,9 +30,30 @@ class doubly_linked_list{
         new_node.prev = this.tail
         this.tail.next = new_node
         this.tail = new_node
-        
-
     }
+    /** adds a node with value v at specific index idx*/
+    insert_at(idx, v) {
+        let new_node = new Node(v)
+        let i = 0
+        let current = this.head
+        let prv = null
+        if(idx == 0) {
+            new_node.next = this.head
+            this.head.prev = new_node
+            this.head = this.head.prev
+            return
+        }
+        while(i < idx) {
+            prv = current
+            current = current.next
+        }
+        new_node.next = current
+        new_node.prev = prv
+        prv.next = new_node
+        current.prev = new_node
+        this.size++
+    }
+
     print_list(){
         let current = this.head
         while(current){
@@ -45,5 +67,6 @@ let dll = new doubly_linked_list
 dll.append_node(23)
 dll.append_node(2)
 dll.append_node(22)
+dll.insert_at(0,9)
 
 dll.print_list()
